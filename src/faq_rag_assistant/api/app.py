7ae6aed35e8aa448acbff3cda91260c7f9943f30
margleb <margleb.dm@yandex.ru>
2026-09-17 14:09:34 +0300

    сохранение диалога в сессии

--- src/faq_rag_assistant/api/app.py
+++ src/faq_rag_assistant/api/app.py
@@ -1,3 +1,5 @@
+import uuid
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -5,24 +7,39 @@ from faq_rag_assistant.agent.core import run_agent
 
 app = FastAPI(title="FAQ RAG Assistant")
 
+SESSIONS = {}
+
 
 class ChatRequest(BaseModel):
+    session_id: str | None
     message: str
 
 
 class ChatResponse(BaseModel):
+    session_id: str | None
     answer: str
 
 
 @app.post("/chat", response_model=ChatResponse)
 def chat(request: ChatRequest):
-    messages = [
+
+    if request.session_id is None:
+        session_id = str(uuid.uuid4())
+        SESSIONS[session_id] = []
+        messages = SESSIONS[session_id]
+    else:
+        session_id = request.session_id
+        messages = SESSIONS[session_id]
+
+    messages.append(
         {
             "role": "user",
             "content": request.message,
         }
-    ]
+    )
 
     answer, messages = run_agent(messages)
 
-    return ChatResponse(answer=answer)
+    SESSIONS[session_id] = messages
+
+    return ChatResponse(session_id=session_id, answer=answer)
